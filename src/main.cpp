9e7f1419864121003567fa6d3033cadba2db941f
Sze Howe Koh <szehowe.koh@gmail.com>
2018-01-11 20:50:01 +0800

    Remove old test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,11 @@
 #include "widget.h"
 #include <QApplication>
 
-#include <QJsonArray>
-#include <QJsonObject>
-#include <QDebug>
-
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	Widget w;
 	w.show();
-
-/*
-	QJsonArray arr
-	{
-		"Hello",
-		"World",
-		QJsonObject
-		{
-			{"str1", "Laa"}
-		}
-	};
-	QJsonValueRefPtr ptr(&arr, 0);
-
-	qDebug() << arr;
-	*ptr = "Whee";
-
-	qDebug() << arr[2] << arr[2].isObject();
-
-	arr[2].toObject();
-
-	// ... Can't get a QJsonValueRef to an nested structure... :-(
-	qDebug() << arr;
-*/
 
 	return a.exec();
 }
